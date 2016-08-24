3cf6b949131e74155b70c4614c7b19bf1081d992
nicosesma <nicosm310@gmail.com>
2016-08-24 08:48:37 -0700

    deleted users route

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 
 const routes = require('./routes/index')
-const users = require('./routes/users')
 
 const app = express()
 
@@ -23,7 +22,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
-app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use( (req, res, next) => {
